091496e577af9b40d1f8db3977128a61b4fde659
donmilorad87 <donmilorad87@gmail.com>
2024-03-22 12:54:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { createClient } from 'redis';
+
+const app = express();
+
+
+
+const client = createClient({
+  host:'redis-server',
+  port: 6379
+});
+
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081');
+});
